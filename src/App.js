57100f34a7c36b457913f20a640a3f1b6d8abae1
Levi Bernard <levichristianbernard@gmail.com>
2020-04-01 21:00:28 -0500

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,70 +1,69 @@
 import React from 'react';
-import { Toast } from 'react-bootstrap'
+import { Toast } from 'react-bootstrap';
 import './App.css';
-
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      show: false,
+      show: false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log('submit button worked')
-    this.setState({ show: true })
+    console.log('submit button worked');
+    this.setState({ show: true });
   }
 
   render() {
     return (
-      <div className="App">
-
-        <div className="App-header">
-          <h1 className="mb-5">Add Property Address</h1>
+      <div className='App'>
+        <div className='App-header'>
+          <h1 className='mb-5'>Add Property Address</h1>
           <form onSubmit={this.handleSubmit}>
-            <div className="mb-4">
-              <input id="username" name="username" type="text" className="p-1" />
+            <div className='mb-4'>
+              <input
+                id='username'
+                name='username'
+                type='text'
+                className='p-1'
+              />
             </div>
-            <div className="submitButtonContainer ">
-              <button
-                className="submitButton btn btn-primary mb-3"
-              >
+            <div className='submitButtonContainer '>
+              <button className='submitButton btn btn-primary mb-3'>
                 Submit
-                        </button>
+              </button>
             </div>
           </form>
           <div>
             <Toast
-              id="toast"
+              id='toast'
               show={this.state.show}
               onClose={() => this.setState({ show: false })}
               delay={1000}
-              animation={true}
+              // animation={true}
               autohide
-            // style={{
-            //   position: 'absolute',
-            //   top: 10,
-            //   right: 10,
-            //   width: 300
-            // }}
+              // style={{
+              //   position: 'absolute',
+              //   top: 10,
+              //   right: 10,
+              //   width: 300
+              // }}
             >
-              <Toast.Header>
-                <img src="" className="rounded mr-2" alt="" className="mt-3" />
-                <strong className="mr-auto text-primary">Success!</strong>
-                <small>just now</small>
-              </Toast.Header>
-              <Toast.Body >Your property has been saved</Toast.Body>
+              {/* <Toast.Header>
+                <img src='' className='rounded mr-2' alt='' className='mt-3' />
+                <strong className='mr-auto text-primary'></strong>
+                <small></small>
+              </Toast.Header> */}
+              <Toast.Body>
+                <strong>Your property has been saved</strong>
+              </Toast.Body>
             </Toast>
-
           </div>
         </div>
-
-      </div >
-
-
+      </div>
     );
   }
 }
