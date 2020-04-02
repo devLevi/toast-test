55009fee8e0ef38c31ca8bdc8c3c1066cf97700d
devLevi <levichristianbernard@gmail.com>
2020-04-01 20:26:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Toast } from 'react-bootstrap'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('submit button worked')
+    this.setState({ show: true })
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="App-header">
+          <h1 className="mb-5">Add Property Address</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className="mb-4">
+              <input id="username" name="username" type="text" className="p-1" />
+            </div>
+            <div className="submitButtonContainer ">
+              <button
+                className="submitButton btn btn-primary mb-3"
+              >
+                Submit
+                        </button>
+            </div>
+          </form>
+          <div>
+            <Toast
+              id="toast"
+              show={this.state.show}
+              onClose={() => this.setState({ show: false })}
+              delay={1000}
+              animation={true}
+              autohide
+            // style={{
+            //   position: 'absolute',
+            //   top: 10,
+            //   right: 10,
+            //   width: 300
+            // }}
+            >
+              <Toast.Header>
+                <img src="" className="rounded mr-2" alt="" className="mt-3" />
+                <strong className="mr-auto text-primary">Success!</strong>
+                <small>just now</small>
+              </Toast.Header>
+              <Toast.Body >Your property has been saved</Toast.Body>
+            </Toast>
+
+          </div>
+        </div>
+
+      </div >
+
+
+    );
+  }
 }
 
 export default App;
